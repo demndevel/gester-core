9bc9283ba29ef722593a11c9b80f85207957165d
demn <demndevel@gmail.com>
2024-11-25 16:26:14 +0300

    small fixes

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,41 +1,41 @@
 plugins {
-    id("com.android.library")
-    kotlin("android")
-    id("kotlin-parcelize")
+	id("com.android.library")
+	kotlin("android")
+	id("kotlin-parcelize")
 }
 
 android {
-    namespace = "com.demn.gester.core"
-    compileSdk = 34
+	namespace = "io.github.demndevel.gester.core"
+	compileSdk = 34
 
-    defaultConfig {
-        minSdk = 29
+	defaultConfig {
+		minSdk = 29
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		consumerProguardFiles("consumer-rules.pro")
+	}
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    buildFeatures {
-        aidl = true
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+	buildTypes {
+		release {
+			isMinifyEnabled = false
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro"
+			)
+		}
+	}
+	buildFeatures {
+		aidl = true
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
+	kotlinOptions {
+		jvmTarget = "1.8"
+	}
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+	testImplementation(kotlin("test"))
 }
